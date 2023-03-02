eac356de28c8b2c69ae339d798507c839eaece8c
RJ Nader <richard.nader@fynydd.com>
2023-03-02 13:27:13 -0500

    Added some two-way input binding.

--- BuildingComponents/BuildingComponents/Data/WeatherForecastService.cs
+++ BuildingComponents/BuildingComponents/Data/WeatherForecastService.cs
@@ -4,12 +4,12 @@ namespace BuildingComponents.Data
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(0, 4).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
